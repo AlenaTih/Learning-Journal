d59b58c2de7a2334598439363901d372f03a6aa7
Alena <alyonatihomirova9@gmail.com>
2024-01-09 18:44:44 +0300

    update

--- footer.js
+++ footer.js
@@ -1,5 +1,9 @@
+const date = new Date()
+
+const year = date.getFullYear()
+
 const footer = `
-<h4>My Learning Journal</h4>
-<h6>Copyright ©2024</h6>`
+    <h4>My Learning Journal</h4>
+    <h6>Copyright ©${year}</h6>`
 
 export default footer
