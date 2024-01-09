bdadd58ae05d48a08e97282bd2851a0686a95086
Alena <alyonatihomirova9@gmail.com>
2024-01-10 02:30:07 +0300

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import footer from '/footer.js'
 const mobileNavHome = document.getElementById("mobile-nav-home")
 const mobileNavPosts = document.getElementById("mobile-nav-posts")
 const mobileNavMe = document.getElementById("mobile-nav-me")
+const mobileNav = document.querySelector(".mobile-nav")
 
 const moreRecentPostsFeed = document.getElementById("recent-posts-more-feed")
 
@@ -29,34 +30,39 @@ document.addEventListener("click", function(e) {
     }
 })
 
+window.addEventListener("click", rootClick)
+
 function showMenuHome() {
     mobileNavHome.innerHTML = `
-    <div class="mobile-menu-container">
+    <div class="mobile-menu-container" id="mobile-menu-container-home">
         <ul>
             <li><a href="posts.html">Recent posts</a></li>
             <li><a href="about-me.html">About me</a></li>
         <ul>
     </div>`
+    document.getElementById("mobile-menu-container-home").style.display = "flex"
 }
 
 function showMenuPosts() {
     mobileNavPosts.innerHTML = `
-    <div class="mobile-menu-container">
+    <div class="mobile-menu-container" id="mobile-menu-container-posts">
         <ul>
             <li><a href="index.html">Home</a></li>
             <li><a href="about-me.html">About me</a></li>
         <ul>
     </div>`
+    document.getElementById("mobile-menu-container-posts").style.display = "flex"
 }
 
 function showMenuAbout() {
     mobileNavMe.innerHTML = `
-    <div class="mobile-menu-container">
+    <div class="mobile-menu-container" id="mobile-menu-container-me">
         <ul>
             <li><a href="index.html">Home</a></li>
             <li><a href="posts.html">Recent posts</a></li>
         <ul>
     </div>`
+    document.getElementById("mobile-menu-container-me").style.display = "flex"
 }
 
 function showMorePosts() {
@@ -86,9 +92,47 @@ function showEvenMorePosts() {
 
     morePosts.push(newPost)
     morePosts.push(newPost)
-    morePosts.push(newPost)    
+    morePosts.push(newPost)
 
     showMorePosts()
+}
+
+function rootClick(e) {
+    if (mobileNavHome &&
+        document.getElementById("mobile-menu-container-home").style.display === "flex" &&
+        e.target.id !== "mobile-nav-home" &&
+        e.target.id !== "hamburger-home" &&
+        e.target.id !== "mobile-menu-container-home" &&
+        !mobileNavHome.contains(e.target)) {
+        // Element clicked was not the mobile nav or its children — close the mobile nav
+        document.getElementById("mobile-menu-container-home").style.display = "none"
+        mobileNav.innerHTML = `
+        <i class="fa-solid fa-bars" id="hamburger-home"></i>`
+        } else if (
+            mobileNavPosts &&
+            document.getElementById("mobile-menu-container-posts").style.display === "flex" &&
+            e.target.id !== "mobile-nav-posts" &&
+            e.target.id !== "hamburger-posts" &&
+            e.target.id !== "mobile-menu-container-posts" &&
+            !mobileNavPosts.contains(e.target)
+        ) {
+            // Element clicked was not the mobile nav or its children — close the mobile nav
+            document.getElementById("mobile-menu-container-posts").style.display = "none"
+            mobileNav.innerHTML = `
+            <i class="fa-solid fa-bars" id="hamburger-posts"></i>`
+        } else if (
+            mobileNavMe &&
+            document.getElementById("mobile-menu-container-me").style.display === "flex" &&
+            e.target.id !== "mobile-nav-me" &&
+            e.target.id !== "hamburger-about" &&
+            e.target.id !== "mobile-menu-container-me" &&
+            !mobileNavMe.contains(e.target)
+        ) {
+            // Element clicked was not the mobile nav or its children — close the mobile nav
+            document.getElementById("mobile-menu-container-me").style.display = "none"
+            mobileNav.innerHTML = `
+            <i class="fa-solid fa-bars" id="hamburger-about"></i>`
+        }
 }
 
 function renderFooter() {
